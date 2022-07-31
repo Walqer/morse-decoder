69835dad9f9c2bbf83e83fa6439a1c9b59629491
Alimossim <agaev.alimosim@gmail.com>
2022-07-31 16:35:59 +0600

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -35,12 +35,45 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    'space':  ' ',
 };
 
+const MORSE_BINAR = {
+    '10': '.',
+    '11': '-',
+    '*': 'space',
+} ;
+
 function decode(expr) {
-    // write your solution here
+   let result = [];
+   let morse;
+   let local = '';
+   for(let i = 0; i < expr.length; i+= 10){
+    let acc = expr.substr(i,10);
+    acc = [acc.slice(acc.indexOf('1'))];
+    result.push(acc);
+    
+
+   }
+  morse = result.map(function(item){
+    let string = '';
+    for(let i = 0; i < item[0].length; i+=2){
+        string += MORSE_BINAR[item[0].substr(i, 2)];
+        
+    }
+    return string;
+   });
+
+  result = morse.map(function(item){
+    return (MORSE_TABLE[item]) ;
+   });
+  return (result.join(''));
+  
+
+
 }
 
 module.exports = {
     decode
-}
+};
+
